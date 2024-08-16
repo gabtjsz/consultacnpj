e81193a1c06fa6ea2c95d5816f287adb54b3e23b
Larissa <annams123456@gmail.com>
2024-08-16 16:22:56 -0300

    primeira versao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+//BLOCO DE IMPORTAÇÃO
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './style.css';
+import api from "./services/api"
 
-function App() {
+function App (){
+  //atribui um nome para uma constante e indica o valor da sua variável
+  const [input, setInput] = useState('');
+  const [cnpj, setCnpj] = useState({});
+
+  //ASYNC FUNCTION: função assícrona para trazer informações ao api
+  async function handleSearch(){
+    //IF (INPUT === ''): verificando se o usuário escreveu o cep, caso nao, emite um alerta pedindo para fornecer um cep
+    if(input === ''){
+      alert ("Preenha algum CNPJ")
+      return;
+    }
+    //TRY: executa a ação desejada, consullta api e armazena a informação 
+    try{
+      const response = await api.get(`${input}`)
+      setCnpj(response.data)
+      setInput("")
+
+
+    //CATCH: sempre que a aplicação 'try' não der como esperado, emite um alerta
+    }catch{
+      alert("Erro ao buscar CNPJ")
+      setInput("")
+    }
+  }
+
+  //RETURN: retorna a aquisição
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Consultar CNPJ</h1>
+
+  <div className="containerInput">
+      <input
+      type="text"
+      placeholder="Digite o CNPJ..."
+      //value:  valor do INPUT
+      value={input}
+
+    //ON CHANGE: captar qualquer tecla que estiver na variavel input
+    //SET INPUT: allter o valor da variavel forneecida pelo usuário 
+      onChange={(e) => setInput(e.target.value)} 
+      />
+
+      <button className="buttonSearch" onClick= {handleSearch}>
+      
+        <FiSearch size={25} color="FFF"/>
+      </button> 
+    </div>
+    {Object.keys(cnpj).length > 0 && (
+      <main className="main">
+        <h2>Razão Social: {cnpj.razao_social}</h2>
+        <span>Fundação: {cnpj.data_inicio_atividade}</span>
+        <span>Situação Cadastral: {cnpj.desrição_situacao_cadastral}</span>
+        <span>Contato: {cnpj.ddd_fax}</span>
+      </main>
+    )}  
+
     </div>
   );
 }
 
 export default App;
+
+
+//Object.keys(cep).length > 0 &&: se CEP pr maior que 0, será renderizado
+//ONCLICK; quando o usuário apertar o botão, vai chamar a função HANDLESEARCH
